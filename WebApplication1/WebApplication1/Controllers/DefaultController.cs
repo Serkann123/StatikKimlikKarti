339cd71f1ad2ff87c7f3d5bdaab168d1136f284c
serkanaltunay0203 <serkanaltunay0203@gmail.com>
2024-11-28 17:36:41 +0300

    .

--- WebApplication1/WebApplication1/Controllers/DefaultController.cs
+++ WebApplication1/WebApplication1/Controllers/DefaultController.cs
@@ -8,7 +8,7 @@ namespace WebApplication1.Controllers
 {
     public class DefaultController : Controller
     {
-        // GET: Default
+     
         public ActionResult Index()
         {
             return View();
